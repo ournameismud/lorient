c6a310bef8f69d90842fb8d617e8a0c718771050
Cole Henley <cole@ournameismud.co.uk>
2018-09-18 17:38:07 +0100

    updated 0.0.3 > 0.0.4

--- src/Lorient.php
+++ src/Lorient.php
@@ -59,7 +59,7 @@ class Lorient extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '0.0.3';
+    public $schemaVersion = '0.0.4';
 
     // Public Methods
     // =========================================================================
